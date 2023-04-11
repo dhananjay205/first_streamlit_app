29c3e21d320f914c1dede53d654408b20151bc9d
dhananjay205 <130467237+dhananjay205@users.noreply.github.com>
2023-04-11 09:31:22 -0400

    Update streamlit_app.py
    Add line to import streamlit
    added a title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('brekfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlite.text('Kale,Spinch & Rocket Smoothie')
+streamlite.text('Hard-Boiled Free-Range EGG')
